d53fe6e04651aca1f69a83bc77a9b20d9f778c6f
Anmol Tariq <anmoltariqbutt@gmail.com>
2019-04-26 22:43:42 +0500

    category||Delivery_Team

---
+++ OnlineFoodCorner/OnlineFoodCorner/Controllers/CategoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineFoodCorner;
+
+namespace OnlineFoodCorner.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private DB24Entities db = new DB24Entities();
+
+        // GET: Categories
+        public ActionResult ManageStock()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryId,CategoryName,Description")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("ManageStock");
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryId,CategoryName,Description")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ManageStock","Categories");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("ManageStock");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OnlineFoodCorner/OnlineFoodCorner/Controllers/Delivery_TeamController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineFoodCorner;
+
+namespace OnlineFoodCorner.Controllers
+{
+    public class Delivery_TeamController : Controller
+    {
+        private DB24Entities db = new DB24Entities();
+
+        // GET: Delivery_Team
+        public ActionResult ManageDeliveryTeam()
+        {
+            return View(db.Delivery_Teams.ToList());
+        }
+
+        // GET: Delivery_Team/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Delivery_Team/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Create_Date")] Delivery_Team delivery_Team)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                db.Delivery_Teams.Add(delivery_Team);
+                db.SaveChanges();
+                return RedirectToAction("ManageDeliveryTeam");
+            }
+
+            return View(delivery_Team);
+        }
+
+        // GET: Delivery_Team/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Delivery_Team delivery_Team = db.Delivery_Teams.Find(id);
+            if (delivery_Team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(delivery_Team);
+        }
+
+        // POST: Delivery_Team/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Create_Date")] Delivery_Team delivery_Team)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                db.Entry(delivery_Team).State = EntityState.Modified;
+                
+                db.SaveChanges();
+                return RedirectToAction("ManageDeliveryTeam");
+            }
+            return View(delivery_Team);
+        }
+
+        // GET: Delivery_Team/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Delivery_Team delivery_Team = db.Delivery_Teams.Find(id);
+            if (delivery_Team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(delivery_Team);
+        }
+
+        // POST: Delivery_Team/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Delivery_Team delivery_Team = db.Delivery_Teams.Find(id);
+            db.Delivery_Teams.Remove(delivery_Team);
+            db.SaveChanges();
+            return RedirectToAction("ManageDeliveryTeam");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
